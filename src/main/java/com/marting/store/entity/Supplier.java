57db8f0571a03117bc7b8771a0a1d43a39a1aa3c
martin.galvan@kinandcarta.com <marting@Martins-MacBook-Pro.local>
2022-04-17 23:37:41 -0500

    Minor fixes 2

--- src/main/java/com/marting/store/entity/Supplier.java
+++ src/main/java/com/marting/store/entity/Supplier.java
@@ -2,7 +2,11 @@ package com.marting.store.entity;
 
 import com.marting.store.entity.abstractEntities.User;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@Table
 public class Supplier extends User implements Serializable {
 }
